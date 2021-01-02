59e23300000f7f392105cf688410b9368716aa19
Edward Spencer <epfspencer@yahoo.com>
2021-01-02 21:31:43 +0000

    Add entry page
    Users can now load any wiki page, and the app will attempt to load the
    appropriate page. Markdown data is loaded directly, if found, otherwise
    an appropriate warning is shown.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def load_wiki(request, title):
+    content = util.get_entry(title)
+    return render(request, f"encyclopedia/wiki.html", {
+        "content": content,
+        "title": title,
+    })
